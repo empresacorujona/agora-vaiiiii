dff31f43a0e59d19d52a449c414f39f515b8288c
Christopher Gabriel <christophemarques995@gmail.com>
2026-06-16 15:03:41 -0300

    dsa

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_templates
 
 app = Flask("__main__")
 
 @app.route("/")
 def main():
-    return "pagina inicial"
+    return render_templates("paginainicial.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
